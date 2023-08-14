74bb7c4faa75139d9108044b49d37f955320ed01
Alexander <alexanderattar@gmail.com>
2023-08-14 16:20:41 -0400

    Add structured error log and start building bloom filter logic

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use environment::Environment;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::time::sleep;
 
-use chrono::Utc;
 use colored::*;
 use ethers::abi::Abi;
 use ethers::contract::Contract;
@@ -76,6 +77,34 @@ enum MatchField {
     ComplexMatch(std::collections::HashMap<String, String>), // This replaces the {[key: string]: string} object structure
 }
 
+// TODO: Move types to their own files
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+enum EventType {
+    TransferERC20,
+    TransferERC721,
+    TransferSingleERC1155,
+    TransferBatchERC1155,
+    BridgeableContractDeployed,
+    HolographableContractEvent,
+    CrossChainMessageSent,
+    AvailableOperatorJob,
+    FinishedOperatorJob,
+    FailedOperatorJob,
+}
+
+enum ContractType {
+    ERC20,
+    ERC721,
+    ERC1155,
+}
+
+enum BloomType {
+    Topic,
+}
+
+type BloomFilter = Vec<u8>; // Placeholder type. This should be replaced with the actual data type for a bloom filter in Rust.
+type BloomFilterMap = HashMap<EventType, BloomFilter>;
+
 const TIMEOUT_THRESHOLD: u64 = 60_000;
 const ZERO: i64 = 0;
 const ONE: i64 = 1;
@@ -110,6 +139,7 @@ struct NetworkMonitor {
     providers: HashMap<String, Arc<Provider<Http>>>,
     holograph_addresses: HashMap<Environment, Address>,
     contracts: HashMap<String, ContractInstance<Arc<Provider<Http>>, Provider<Http>>>,
+    bloom_filters: BloomFilterMap,
 }
 
 impl NetworkMonitor {
@@ -121,6 +151,7 @@ impl NetworkMonitor {
             providers: HashMap::new(),
             holograph_addresses: addresses,
             contracts: HashMap::new(),
+            bloom_filters: HashMap::new(),
         }
     }
 
@@ -273,6 +304,152 @@ impl NetworkMonitor {
         }
 
         Ok(())
+    }
+
+    fn build_filter(
+        &self,
+        bloom_type: BloomType,
+        event_type: EventType,
+        target_address: Option<String>,
+        contract_type: Option<ContractType>,
+    ) -> BloomFilter {
+        // Placeholder
+        vec![]
+    }
+
+    fn filter_builder(&mut self) {
+        let build_event_filter =
+            |event_type: EventType,
+             contract_name: Option<&str>,
+             contract_type: Option<ContractType>| {
+                let address = contract_name
+                    .and_then(|name| self.contracts.get(name))
+                    .map(|contract| contract.address().to_string());
+
+                self.build_filter(BloomType::Topic, event_type, address, contract_type)
+            };
+
+        // Build all filters first into a temporary vector
+        let mut all_filters = vec![
+            (
+                EventType::TransferERC20,
+                build_event_filter(EventType::TransferERC20, None, Some(ContractType::ERC20)),
+            ),
+            (
+                EventType::TransferERC721,
+                build_event_filter(EventType::TransferERC721, None, Some(ContractType::ERC721)),
+            ),
+            (
+                EventType::TransferSingleERC1155,
+                build_event_filter(
+                    EventType::TransferSingleERC1155,
+                    None,
+                    Some(ContractType::ERC1155),
+                ),
+            ),
+            (
+                EventType::TransferBatchERC1155,
+                build_event_filter(
+                    EventType::TransferBatchERC1155,
+                    None,
+                    Some(ContractType::ERC1155),
+                ),
+            ),
+            (
+                EventType::BridgeableContractDeployed,
+                build_event_filter(EventType::BridgeableContractDeployed, Some("factory"), None),
+            ),
+            (
+                EventType::HolographableContractEvent,
+                build_event_filter(EventType::HolographableContractEvent, Some("registry"), None),
+            ),
+            (
+                EventType::CrossChainMessageSent,
+                build_event_filter(EventType::CrossChainMessageSent, Some("operator"), None),
+            ),
+            (
+                EventType::AvailableOperatorJob,
+                build_event_filter(EventType::AvailableOperatorJob, Some("operator"), None),
+            ),
+            (
+                EventType::FinishedOperatorJob,
+                build_event_filter(EventType::FinishedOperatorJob, Some("operator"), None),
+            ),
+            (
+                EventType::FailedOperatorJob,
+                build_event_filter(EventType::FailedOperatorJob, Some("operator"), None),
+            ),
+        ];
+
+        // Then insert filters into the hashmap
+        for (event, filter) in all_filters.drain(..) {
+            self.bloom_filters.insert(event, filter);
+        }
+    }
+
+    // Generic retry function
+    async fn retry<F, T>(
+        &self,
+        network: &str,
+        func: F,
+        attempts: usize,
+        interval: u64,
+    ) -> Result<T, Box<dyn std::error::Error>>
+    where
+        F: Fn() -> std::pin::Pin<
+            Box<dyn std::future::Future<Output = Result<T, Box<dyn std::error::Error>>>>,
+        >,
+    {
+        for i in 0..attempts {
+            match func().await {
+                Ok(result) => return Ok(result),
+                Err(e) => {
+                    self.structured_log_error(network, &e.to_string());
+                    if i == attempts - 1 {
+                        return Err(Box::new(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!(
+                                "Maximum attempts reached, function did not succeed after {} attempts",
+                                attempts
+                            ),
+                        )));
+                    }
+                    sleep(Duration::from_millis(interval)).await;
+                }
+            }
+        }
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Unexpected error in retry loop",
+        )))
+    }
+
+    fn structured_log_error(&self, network: &str, msg: &str) {
+        let timestamp = chrono::Utc::now().format("%+").to_string();
+        let timestamp_color = "red"; // Changed to red for error logging
+
+        // Using the provided network instead of inferring from the providers
+        let network_name =
+            network.chars().nth(0).unwrap_or_default().to_uppercase().to_string() + &network[1..];
+
+        let env_name = match Self::get_env() {
+            Ok(env) => format!("{:?}", env),
+            Err(_) => "UnknownEnv".to_string(),
+        };
+
+        // For errors we're prepending the tag with [ERROR]
+        let tag_string = format!("[ERROR] ");
+
+        let log_message = format!(
+            "[{}] [{}] [{}] {}{}",
+            timestamp.color(timestamp_color),
+            network_name.color("blue"),
+            env_name.color("cyan"),
+            tag_string,
+            msg.trim_start() // Remove leading whitespaces from the message
+        );
+
+        println!("{}", log_message);
     }
 
     fn structured_log(&self, msg: &str, tag_id: Option<&dyn ToString>) {
